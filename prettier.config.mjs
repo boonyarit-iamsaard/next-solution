7b87ed4571d0fbff65b1b6a63f0d6a0a2760a89c
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-03-20 20:57:52 +0700

    chore: configure prettier with eslint and typescript

---
+++ prettier.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+const config = {
+  singleQuote: true,
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '',
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/app/(.*)$',
+    '^@/assets/(.*)$',
+    '^@/common/(.*)$',
+    '^@/core/(.*)$',
+    '^@/features/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+};
+
+export default config;
