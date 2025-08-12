b252f01e2004bb07985ca033168004f68b8547f5
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-08-12 11:51:05 +0700

    chore: update root eslint config ignore patterns

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,10 @@ import { baseConfig } from '@next-solution/eslint-config'
 
 export default baseConfig.append({
   ignores: [
+    'apps/*',
+    'packages/*',
+    'tools/*',
+    '!tools/typescript-config',
     'pnpm-lock.yaml',
   ],
 })
